0fa2ccc7d853be021c8e75c323228be94ac517de
Kurt Seifried <kurt@seifried.org>
2020-10-22 10:23:03 -0600

    updated data script

--- scripts/get-spf-domain-ip-ranges.py
+++ scripts/get-spf-domain-ip-ranges.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
+# Hardcoded to Mailgun for now
+
 # Get list of network blocks
 
 from SPF2IP import SPF2IP
 lookup = SPF2IP('mailgun.org')
 
 IP_blocks=lookup.IPArray('4')
+
+with open('mailgun.org-spf.txt', 'w') as f:
+    for item in IP_blocks:
+        f.write("%s\n" % item)
 
 # Convert Network blocks to IP_blocks and iterate over them with pydnsbl
 
